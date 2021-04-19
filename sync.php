e6bb42ce96fae81dacb9720904c2393e7381c1c7
Hazrul <hi@hazrulazhar.com>
2021-04-19 16:09:12 +0800

    add sync on push

---
+++ sync.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+
+if ( $_GET['sync']==1 ) {
+  shell_exec( 'cd /home/hazrulaz/www/w4l2 && git reset --hard HEAD && git pull' );
+}
+
+?>
