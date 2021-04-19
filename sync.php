64efd7f8afa15f9fe0d9d8639424582da636b148
Hazrul <hi@hazrulazhar.com>
2021-04-19 16:18:00 +0800

    force something to appear

--- sync.php
+++ sync.php
@@ -3,7 +3,8 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_GET['sync']==1 ) {
-  echo shell_exec( 'cd /home/hazrulaz/www/w4l2 && git reset --hard HEAD && git pull' );
+    $output = shell_exec( 'cd /home/hazrulaz/www/w4l2 && git reset --hard HEAD && git pull' );
+    echo "results: ".$output;
 }
 
 ?>
