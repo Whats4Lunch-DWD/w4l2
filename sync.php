9353affb5001bb9af9358c27c4319bf524f7230a
Hazrul <hi@hazrulazhar.com>
2021-04-19 16:15:40 +0800

    output the shell exec

--- sync.php
+++ sync.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
 
 if ( $_GET['sync']==1 ) {
-  shell_exec( 'cd /home/hazrulaz/www/w4l2 && git reset --hard HEAD && git pull' );
+  echo shell_exec( 'cd /home/hazrulaz/www/w4l2 && git reset --hard HEAD && git pull' );
 }
 
 ?>
