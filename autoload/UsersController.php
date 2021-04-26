294c4773e7ed77cebe3262a53768510a84528d6c
Hazrul <hi@hazrulazhar.com>
2021-04-26 22:40:27 +0800

    sign up page

--- autoload/UsersController.php
+++ autoload/UsersController.php
@@ -72,7 +72,7 @@ class UsersController {
     }
 
     public function checkUsername($username) {
-        $this->mapper = new DB\SQL\Mapper($f3->get('DB'),"users");	// create DB query mapper object		
+        $this->mapper = new DB\SQL\Mapper($this->f->get('DB'),"users");	// create DB query mapper object		
         $user = $this->mapper->load(array("username = ?",$username));
 
         if (sizeof($user)<1) {
