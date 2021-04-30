ffc2144f8925542ca9b8a36d6924f893554eecd9
Hazrul <hi@hazrulazhar.com>
2021-04-30 18:30:16 +0800

    save favourites

--- autoload/TransactionsController.php
+++ autoload/TransactionsController.php
@@ -5,12 +5,14 @@
 class TransactionsController {
 	private $mapper;
     private $cart_items_mapper;
+    private $user_mapper;
     
 	public function __construct() {
 		global $f3;
 
         $this->mapper = new DB\SQL\Mapper($f3->get('DB'),"transactions");	// create DB query mapper object
         $this->cart_items_mapper = new DB\SQL\Mapper($f3->get('DB'),"cart_items");
+        $this->user_mapper = new DB\SQL\Mapper($f3->get('DB'),"users");
     }
     
     public function add($cart) {
@@ -20,7 +22,7 @@ class TransactionsController {
 
         foreach($cart as $cart_key => $cart_value) {
             if ($cart_key=="saveaddress" or $cart_key=="saveorder") {
-                // do nothing
+                // Do nothing
             } else {
                 if ($cart_key!="address2") {
                     if ($cart_value=="") {
@@ -28,6 +30,10 @@ class TransactionsController {
                     }
                 }
                 $this->mapper[$cart_key]=$cart_value;
+
+                if ($cart["saveaddress"]==1 and $cart_key["address1"]!="") {
+                    $this->saveAddressToProfile($cart_value);
+                }
             }
         }
         $this->mapper["status"]="in_progress";
@@ -41,6 +47,11 @@ class TransactionsController {
         $_SESSION["CART_SESSION"]=null;
         $this->mapper->save();
 
+
+        if ($cart["saveorder"]=="1") {
+            $this->saveOrderToFavs($this->mapper["id"]);
+        }
+
         return $this->mapper["id"];
     }
 
@@ -51,4 +62,17 @@ class TransactionsController {
         return $transaction_cart_items;
     }
 
+
+    public function saveAddressToProfile($address) {
+        
+        $arr_location = explode(" ", $address);
+        $pcode = array_pop($arr_location);
+        $this->user_mapper["address1"]=$address;
+        $this->user_mapper["postalcode"]=$pcode;
+        $this->user_mapper->save();
+    }
+
+    public function saveOrderToFavs($cart_id) {
+
+    }
 }
