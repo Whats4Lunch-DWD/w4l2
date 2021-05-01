a1627de83d98d14c1038615421e5912227a661d4
Hazrul <hi@hazrulazhar.com>
2021-05-01 10:52:17 +0800

    edit profile form

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ $f3->route('GET /profile/edit',
 $f3->route('POST /profile/edit',
   function ($f3) {
     $controller = new UsersController;
-    $data = $controller->updateProfile($_POST["id"]);
+    $data = $controller->updateProfile($_POST);
     $f3->set("user",$data);
     $f3->set('html_title','Edit Profile - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
     $f3->set('content','edit_profile.html');
