16f85b4f7a34ee0b029455c377f5bffac4d9a180
Hazrul <hi@hazrulazhar.com>
2021-05-02 17:48:17 +0800

    orders init

--- index.php
+++ index.php
@@ -420,6 +420,8 @@ $f3->route('GET /admin/restaurants/show/@id',
 $f3->route('GET /admin/orders',
   function ($f3) {
     $controller = new TransactionsController;
+    $transactions = $controller->listTransactions();
+    $f3->set('transactions',$transactions);
     $f3->set('html_title','Manage Orders - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
     $f3->set('content','admin/orders.html');
     echo Template::instance()->render('layout.html');
