5bca41db2b18a2cd3fd916f1ea3b6a02f580f9a1
Hazrul <hi@hazrulazhar.com>
2021-05-02 12:47:50 +0800

    test why add menu template not showing

--- index.php
+++ index.php
@@ -407,9 +407,9 @@ $f3->route('GET /admin/orders',
   }
 );
 
-$f3->route('GET /admin/add_menu?restaurant=@id',
+$f3->route('GET /admin/add_menu',
   function ($f3,$args) {
-    $f3->set('resto_id',$args['id']);
+    //$f3->set('resto_id',$args['id']);
     $f3->set('html_title','Add Menu - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
     $f3->set('content','admin/add_menu.html');
     echo Template::instance()->render('layout.html');
