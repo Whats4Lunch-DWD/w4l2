6e15698ba4f7849035f6e2530fb0f0d228b3641a
Hazrul <hi@hazrulazhar.com>
2021-05-02 12:46:08 +0800

    init add menu

--- index.php
+++ index.php
@@ -407,6 +407,15 @@ $f3->route('GET /admin/orders',
   }
 );
 
+$f3->route('GET /admin/add_menu?restaurant=@id',
+  function ($f3,$args) {
+    $f3->set('resto_id',$args['id']);
+    $f3->set('html_title','Add Menu - Whats4Lunch - The World\'s easiest Food Delivery for people with diets and allergies');
+    $f3->set('content','admin/add_menu.html');
+    echo Template::instance()->render('layout.html');
+  }
+);
+
   ////////////////////////
  // Run the F3 engine //
 ////////////////////////
